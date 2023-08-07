80d4b5aa745aea6c20de762c5fca8fad7fd9dbc9
Maria De Fátima <133269366+Maria621@users.noreply.github.com>
2023-08-07 12:08:59 -0300

    Criando CRUD Tema

--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -1,22 +1,36 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { AuthContext } from '../../contexts/AuthContext'
+
 
 
 function Navbar() {
- 
+  let navigate = useNavigate()
+
+  const { usuario, handleLogout } = useContext(AuthContext)
+
+  function logout() {
+      handleLogout()
+      alert('Usuário deslogado com sucesso')
+      navigate('/login')
+  }
+
+  let navbarComponent
   
 
   return (
     <>
      <div className='w-full bg-purple-100 text-white flex justify-center py-4'>
           <div className="container flex justify-between text-lg">
-            <div className='text-2xl font-sans uppercase'>Minas na Área</div>
+          <Link to='/home' className='text-2xl font-bold uppercase'>Blog Pessoal</Link>
 
-            <div className='flex gap-4 font-sans'>
-              <div className='hover:underline'>Mentoria</div>
-              <div className='hover:underline'>Sobre</div>
-              <div className='hover:underline'>Contato</div>
-              <div className='hover:underline rounded bg-green-100 px-2'>Entrar</div>
-              
+            <div className='flex gap-4'>
+            <div className='hover:underline'>Postagens</div>
+            <Link to='/temas' className='hover:underline'>Temas</Link>
+              <div className='hover:underline'>Cadastrar tema</div>
+              <div className='hover:underline'>Perfil</div>
+              <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
+             
             </div>
           </div>
         </div>
