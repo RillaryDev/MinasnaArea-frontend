d418f3c7a63e4ebc18ff6165f7be6bb8bfaf2ef7
lucaolv <lucasbarbato80@gmail.com>
2023-08-08 10:49:42 -0300

    Criando CRUD de Postagem

--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -20,9 +20,9 @@ function Navbar() {
 
   return (
     <>
-     <div className='w-full bg-purple-100 text-white flex justify-center py-4'>
+     <div className='w-full bg-indigo-900 text-white flex justify-center py-4'>
           <div className="container flex justify-between text-lg">
-          <Link to='/home' className='text-2xl font-bold uppercase'>Minas na Area</Link>
+          <Link to='/home' className='text-2xl font-bold uppercase'>Blog Pessoal</Link>
 
             <div className='flex gap-4'>
             <div className='hover:underline'>Postagens</div>
