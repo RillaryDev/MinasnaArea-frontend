df9118104ca1de3f7e4e3a1ed741a6245a5c97b3
RillaryDev <rillarydev@gmail.com>
2023-08-08 11:13:37 -0300

    changing branch

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,11 @@ import Navbar from './components/navBar/NavBar';
 import Footer from './components/footer/Footer';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './paginas/login/Login';
+import Cadastro from './paginas/cadastro/Cadastro';
 import Home from './paginas/home/Home';
 import { AuthProvider } from './contexts/AuthContext';
 import ListaTemas from './components/temas/listaTemas/ListaTemas';
 import FormularioTema from './components/temas/formularioTema/FormularioTema';
-<<<<<<< HEAD
-import Cadastro from './paginas/cadastro/Cadastro';
-
-=======
->>>>>>> d418f3c7a63e4ebc18ff6165f7be6bb8bfaf2ef7
 
 
 function App() {
@@ -21,14 +17,14 @@ function App() {
     <>
     <AuthProvider>
         <BrowserRouter>
-        <Navbar />
+          <Navbar />
           <div className='min-h-[80vh]'>
             <Routes>
               <Route path="/" element={<Login />} />
               <Route path="/login" element={<Login />} />
               <Route path="/cadastro" element={<Cadastro />} />
               <Route path="/home" element={<Home />} />
-              <Route path="/tema" element={<ListaTemas />} />
+              <Route path="/temas" element={<ListaTemas />} />
               <Route path="/cadastroTema" element={<FormularioTema />} />
               <Route path="/editarTema/:id" element={<FormularioTema />} />
 
