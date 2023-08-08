e9e0bd01f411aa16307a6dcd971b9893cee1c257
Israelsilva02 <israel865974@gmail.com>
2023-08-08 10:52:46 -0300

    Atualizando

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,10 @@ import Navbar from './components/navBar/NavBar';
 import Footer from './components/footer/Footer';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './paginas/login/Login';
-<<<<<<< HEAD
 
 import Home from './paginas/home/Home';
 import { AuthProvider } from './contexts/AuthContext';
 import ListaTemas from './components/temas/listaTemas/ListaTemas';
-=======
-import Cadastro from './paginas/cadastro/Cadastro';
-import Home from './paginas/home/Home';
-import { AuthProvider } from './contexts/AuthContext';
->>>>>>> ed493411d1131ddde43f8df752c2a6ea0ffb78df
 
 
 function App() {
@@ -27,14 +21,9 @@ function App() {
             <Routes>
               <Route path="/" element={<Login />} />
               <Route path="/login" element={<Login />} />
-<<<<<<< HEAD
         
               <Route path="/home" element={<Home />} />
               <Route path="/temas" element={<ListaTemas />} />
-=======
-              <Route path="/cadastro" element={<Cadastro />} />
-              <Route path="/home" element={<Home />} />
->>>>>>> ed493411d1131ddde43f8df752c2a6ea0ffb78df
             </Routes>
           </div>
           <Footer />
