1e12c1254b0a4ef5e4796af47cb09f7a5b1ac89d
lucaolv <lucasbarbato80@gmail.com>
2023-08-14 13:40:32 -0300

    deploy

--- src/paginas/cadastro/Cadastro.tsx
+++ src/paginas/cadastro/Cadastro.tsx
@@ -77,105 +77,108 @@ function Cadastro() {
       <div className="bg-pink-400 ">
         <div className="min-h-screen bg-pink-400 text-gray-900 flex justify-center">
           <div className="max-w-screen-1xl m-0 sm:m-10 bg-pink-100 shadow sm:rounded-lg flex justify-center flex-1">
-            <div className="lg:w-1/2 xl:w-5/12 p-6 sm:p-12">
-              <div>
-                <img src="https://i.imgur.com/OJb60Jn.png"
-                  className="w-mx-auto" />
-              </div>
-              <div className="mt-12 flex flex-col items-center">
-                <div className="w-full flex-1 ">
+            <form className='flex justify-center items-center flex-col w-2/3 gap-3' onSubmit={cadastrarNovoUsuario}>
+              <div className="lg:w-1/2 xl:w-5/12 p-6 sm:p-12">
+                <div>
+                  <img src="https://i.imgur.com/OJb60Jn.png"
+                    className="w-mx-auto" />
+                </div>
+                <div className="mt-12 flex flex-col items-center">
+                  <div className="w-full flex-1 ">
 
 
-                  <div className=" text-center self-center mb-6 text-2xl font-base text-gray-800 sm:text-3xl dark:text-white">
-                    Cadastre-se
-                  </div>
+                    <div className=" text-center self-center mb-6 text-2xl font-base text-gray-800 sm:text-3xl dark:text-white">
+                      Cadastre-se
+                    </div>
 
-                  <div className="mx-auto max-w-xs">
-                    <input
-                      className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
-                      type="text"
-                      id="nome"
-                      name="nome"
-                      placeholder="Nome"
-                      value={usuario.nome}
-                      onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)} />
-                    <input
-                      className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5 mb-4 "
-                      id="usuario"
-                      name="usuario"
-                      placeholder="E-mail"
-                      value={usuario.usuario}
-                      onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
-                    />
-                  
+                    <div className="mx-auto max-w-xs">
+                      <input
+                        className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
+                        type="text"
+                        id="nome"
+                        name="nome"
+                        placeholder="Nome"
+                        value={usuario.nome}
+                        onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)} />
+                      <input
+                        className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5 mb-4 "
+                        id="usuario"
+                        name="usuario"
+                        placeholder="E-mail"
+                        value={usuario.usuario}
+                        onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
+                      />
 
-                    <label htmlFor="nome">Tipo de Usuário</label>
-                    <select
-                      className="w-full px-8 py-4 rounded-lg  font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-2"
-                      id="tipo"
-                      name="tipo"
-                      placeholder="Tipo de Usuário"
-                      value={usuario.tipo}
-                      onChange={(e: ChangeEvent<HTMLSelectElement>) => setUsuario({ ...usuario, tipo: e.target.value })}
-                    >
-                      <option value=" ">Mentorada</option>
-                      <option value="mentor">Mentora</option>
-                    </select>
 
-                    <input
-                      className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
-                      type="text"
-                      id="foto"
-                      name="foto"
-                      placeholder="Coloque o link da sua foto aqui"
-                      value={usuario.foto}
-                      onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
-                    />
-                    <input
-                      type="password"
-                      id="senha"
-                      name="senha"
-                      placeholder="Senha"
-                      className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
-                      value={usuario.senha}
-                      onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
-                    />
-                    <input
-                      type="password"
-                      id="confirmarSenha"
-                      name="confirmarSenha"
-                      placeholder="Confirmar Senha"
-                      className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
-                      value={confirmaSenha}
-                      onChange={(e: ChangeEvent<HTMLInputElement>) => handleConfirmarSenha(e)}
-                    />
+                      <label htmlFor="nome">Tipo de Usuário</label>
+                      <select
+                        className="w-full px-8 py-4 rounded-lg  font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-2"
+                        id="tipo"
+                        name="tipo"
+                        placeholder="Tipo de Usuário"
+                        value={usuario.tipo}
+                        onChange={(e: ChangeEvent<HTMLSelectElement>) => setUsuario({ ...usuario, tipo: e.target.value })}
+                      >
+                        <option value=" ">Mentorada</option>
+                        <option value="mentor">Mentora</option>
+                      </select>
 
-                    <button
-                      className="mt-5 tracking-wide font-semibold bg-purple-600 text-white-500 w-full py-4 rounded-lg hover:bg-green-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none">
-                      <svg className="w-6 h-6 -ml-2" fill="none" stroke="currentColor" stroke-width="2"
-                        stroke-linecap="round" stroke-linejoin="round">
-                      </svg>
-                      <span className="mr-3 text-white-100 ">
-                        <p>Cadastrar</p>
-                      </span>
-                    </button>
-                    <p className="mt-6 text-xs text-gray-600 text-center">
-                      Eu li e concordo com os termos de uso da
-                      <a href="#" className="ml-1 border-b border-gray-500 border-dotted">
-                        Política de Privacidade
-                      </a>
-                    </p>
+                      <input
+                        className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
+                        type="text"
+                        id="foto"
+                        name="foto"
+                        placeholder="Coloque o link da sua foto aqui"
+                        value={usuario.foto}
+                        onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
+                      />
+                      <input
+                        type="password"
+                        id="senha"
+                        name="senha"
+                        placeholder="Senha"
+                        className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
+                        value={usuario.senha}
+                        onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
+                      />
+                      <input
+                        type="password"
+                        id="confirmarSenha"
+                        name="confirmarSenha"
+                        placeholder="Confirmar Senha"
+                        className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
+                        value={confirmaSenha}
+                        onChange={(e: ChangeEvent<HTMLInputElement>) => handleConfirmarSenha(e)}
+                      />
+
+                      <button
+                        type='submit'
+                        className="mt-5 tracking-wide font-semibold bg-purple-600 text-white-500 w-full py-4 rounded-lg hover:bg-green-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none">
+                        <svg className="w-6 h-6 -ml-2" fill="none" stroke="currentColor" stroke-width="2"
+                          stroke-linecap="round" stroke-linejoin="round">
+                        </svg>
+                        <span className="mr-3 text-white-100 ">
+                          <p>Cadastrar</p>
+                        </span>
+                      </button>
+                      <p className="mt-6 text-xs text-gray-600 text-center">
+                        Eu li e concordo com os termos de uso da
+                        <a href="#" className="ml-1 border-b border-gray-500 border-dotted">
+                          Política de Privacidade
+                        </a>
+                      </p>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-            <div className="rounded-r-lg flex-1 bg-purple-600 text-center hidden lg:flex">
-              <img className="m-12 xl:m-12  w-[35rem] h-[50rem] bg-contain bg-center bg-no-repeat background-image" src="https://i.imgur.com/PBR25iK.jpg" alt="mentoria" />
-            </div>
+              <div className="rounded-r-lg flex-1 bg-purple-600 text-center hidden lg:flex">
+                <img className="m-12 xl:m-12  w-[35rem] h-[50rem] bg-contain bg-center bg-no-repeat background-image" src="https://i.imgur.com/PBR25iK.jpg" alt="mentoria" />
+              </div>
+              </form>
           </div>
         </div>
-
-      </div>
+      
+    </div >
 
 
     </>
@@ -183,7 +186,3 @@ function Cadastro() {
 }
 
 export default Cadastro
-
-function foto(prevState: null): null {
-  throw new Error('Function not implemented.')
-}
--- src/paginas/perfil/Perfil.tsx
+++ src/paginas/perfil/Perfil.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react'
+import { useContext, useEffect } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { AuthContext } from '../../contexts/AuthContext'
 import { toastAlerta } from '../../util/toastAlerta'
@@ -9,23 +9,15 @@ function Perfil() {
   const { usuario } = useContext(AuthContext)
   const userTipo = usuario.tipo === 'mentor';
 
-  // useEffect(() => {
-  //   if (usuario.token === "") {
-  //     toastAlerta('Dados inconsistentes. Verifique as informações de cadastro.', 'erro')
-  //     navigate("/login")
-  //   }
-  // }, [usuario.token])
+  useEffect(() => {
+    if (usuario.token === "") {
+      toastAlerta('Dados inconsistentes. Verifique as informações de cadastro.', 'erro')
+      navigate("/login")
+    }
+  }, [usuario.token])
 
   return (
 
-    // <div className='container mx-auto mt-4 rounded-2xl overflow-hidden'>
-    //   <img className='w-full h-72 object-cover border-b-8 border-white' src= "" alt="Capa do Perfil" />
-    //   <img src={usuario.foto} alt={`Foto de perfil de ${usuario.nome}`} className='rounded-full w-56 mx-auto mt-[-8rem] border-8 border-white relative z-10' />
-    //   <div className="relative mt-[-6rem] h-72 flex flex-col bg-sky-500 text-white text-2xl items-center justify-center">
-    //     <p>Nome: {usuario.nome} </p>
-    //     <p>Email: {usuario.usuario}</p>
-    //   </div>
-    // </div>
     <div className="bg-pink-100 h-screen py-1">
       <div className="bg-white-100 font-base max-w-2xl mx-4 sm:max-w-sm md:max-w-sm lg:max-w-sm xl:max-w-sm sm:mx-auto md:mx-auto lg:mx-auto xl:mx-auto mt-16 bg-white shadow-xl rounded-lg text-gray-900">
         <div className="rounded-t-lg h-32 overflow-hidden">
